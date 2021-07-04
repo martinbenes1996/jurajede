f6cf8c631f982dc71f8e0b898859b2d09d2622ce
Martin Benes <martinbenes1996@gmail.com>
2021-07-04 21:16:28 +0200

    app added

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import sys
 sys.path.append('src')
 from application import setup_app
 
-def run():
-    app = setup_app()
-    app.run_server(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
+app = setup_app()
 
 if __name__ == '__main__':
-    run()
+    port = os.environ.get("PORT", 33507)
+    app.run_server(host="0.0.0.0", port=port, debug=True)
+    #run()
