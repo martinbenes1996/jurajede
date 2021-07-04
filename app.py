ed2a12de805becea58afbce4cfec13e050dc2f2c
Martin Benes <martinbenes1996@gmail.com>
2021-07-04 21:36:08 +0200

    server address and port

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from application import setup_app
 def run(*args, **kw):
     app = setup_app()
     logging.warning(str(args) + " " + str(kw))
-    app.run_server(*args, **kw)
+    app.run_server(host=args[0]['SERVER_NAME'], port=args[0]['SERVER_PORT'])
     
 # run debug server
-if __name__ == '__main__':
-    app = setup_app()
-    port = os.environ.get("PORT", 33507)
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+#if __name__ == '__main__':
+#    app = setup_app()
+#    port = os.environ.get("PORT", 33507)
+#    app.run_server(host="0.0.0.0", port=port, debug=True)
