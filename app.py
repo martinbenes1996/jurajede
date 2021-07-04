faa7a3f6118548a2d370aee40911ab278434a3bc
Martin Benes <martinbenes1996@gmail.com>
2021-07-04 21:25:54 +0200

    get app

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from application import setup_app
 def run():
     app = setup_app()
     return app
-    #port = os.environ.get("PORT", 33507)
-    #app.run_server(host="0.0.0.0", port=port, debug=True)
     
 if __name__ == '__main__':
-    run()
+    app = setup_app()
+    port = os.environ.get("PORT", 33507)
+    app.run_server(host="0.0.0.0", port=port, debug=True)
