eecbbd80a314cece27f38fd9b97a0192022a3478
Martin Benes <martinbenes1996@gmail.com>
2021-07-04 21:41:49 +0200

    procfile changed

--- app.py
+++ app.py
@@ -4,14 +4,10 @@ import sys
 sys.path.append('src')
 from application import setup_app
 
-# run production server
-def run(*args, **kw):
-    app = setup_app()
-    logging.warning(str(args) + " " + str(kw))
-    app.run_server(host=args[0]['SERVER_NAME'], port=args[0]['SERVER_PORT'])
-    
-# run debug server
-#if __name__ == '__main__':
-#    app = setup_app()
-#    port = os.environ.get("PORT", 33507)
-#    app.run_server(host="0.0.0.0", port=port, debug=True)
+# initialize app object
+app = setup_app()
+
+# run server
+if __name__ == '__main__':
+    port = os.environ.get("PORT", 5000)
+    app.run_server(host="0.0.0.0", port=port, debug=True)
