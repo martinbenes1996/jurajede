e3492ad9c694fce1a179932c1da5ae4b83033604
Martin Benes <martinbenes1996@gmail.com>
2021-07-04 21:25:29 +0200

    do not run app

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import sys
 sys.path.append('src')
 from application import setup_app
 
-app = setup_app()
-
+def run():
+    app = setup_app()
+    return app
+    #port = os.environ.get("PORT", 33507)
+    #app.run_server(host="0.0.0.0", port=port, debug=True)
+    
 if __name__ == '__main__':
-    port = os.environ.get("PORT", 33507)
-    app.run_server(host="0.0.0.0", port=port, debug=True)
-    #run()
+    run()
